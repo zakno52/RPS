216a8a8341d1f8e18b57dcbe98f3931fa436cbf9
zakno52 <101570337+zakno52@users.noreply.github.com>
2024-07-30 10:32:33 +0100

    adding some ui

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 let resultSpace = document.getElementsByTagName("h1")[0];
 let gameButtons = document.querySelectorAll("#gameButton");
 let winCounter=0;
+let loseCounter=0;
+let finalResult = document.createElement("div")
+
 
 Array.from(gameButtons).forEach(button=>{
   button.addEventListener('click', () => {
@@ -10,49 +13,60 @@ Array.from(gameButtons).forEach(button=>{
 
   if (randomNumber<=0.3){
     if (button.innerHTML === "rock") {
-      resultSpace.innerHTML="I choosed rock so do you, its a draw";
+      resultSpace.innerHTML=`I choosed rock, its a draw, you:${winCounter} Com:${loseCounter}`;
     } 
     if (button.innerHTML === "paper") {
-      winCounter+=1
-      resultSpace.innerHTML="I choosed rock, you won  "+`${winCounter}`;
+      resultSpace.innerHTML=`I choosed rock, you:${winCounter+=1} Com:${loseCounter}`;
     }
-    if (button.innerHTML === "cisors") {
-      resultSpace.innerHTML="I choosed rock, you lost";
+    if (button.innerHTML === "scissors") {
+      resultSpace.innerHTML=`I choosed rock, you:${winCounter} Com:${loseCounter+=1}`;
     }
     }
-
 
     if (randomNumber>=0.6){
     if (button.innerHTML === "rock") {
-      resultSpace.innerHTML="I choosed paper, you lost";
+      resultSpace.innerHTML=`I choosed paper, you:${winCounter} Com:${loseCounter+=1}`;
     } 
     if (button.innerHTML === "paper") {
-      resultSpace.innerHTML="I choosed paper so do you, its a draw";
+      resultSpace.innerHTML=`I choosed paper, its a draw, you:${winCounter} Com:${loseCounter}`;
     }
-    if (button.innerHTML === "cisors") {
-      winCounter+=1
-      resultSpace.innerHTML="I choosed paper, you won  "+`${winCounter}`;
+    if (button.innerHTML === "scissors") {
+      resultSpace.innerHTML=`I choosed paper, you:${winCounter+=1} Com:${loseCounter}`;
     }
     }
-
 
     if (randomNumber<0.6 && randomNumber>0.3){
 
     if (button.innerHTML === "rock") {
-      winCounter+=1
-      resultSpace.innerHTML="I choosed cisors, you won  "+`${winCounter}`;
+      resultSpace.innerHTML=`I choosed scissors, you:${winCounter+=1} Com:${loseCounter}`;
     } 
     if (button.innerHTML === "paper") {
-      resultSpace.innerHTML="I choosed cisors, you lost";
+      resultSpace.innerHTML=`I choosed scissors, you:${winCounter} Com:${loseCounter+=1}`;
     }
-    if (button.innerHTML === "cisors") {
-      resultSpace.innerHTML="I choosed cisors so do you, its a draw";
+    if (button.innerHTML === "scissors") {
+      resultSpace.innerHTML=`I choosed scissors so do you, its a draw, you:${winCounter} Com:${loseCounter}`;
     }
     
   }
 
-// reset
-  randomNumber=0;
+  //checking who ever got 5 points first, and announcing whether the player won or lost
+    if (winCounter===5) {
+      finalResult.innerHTML="You won!";
+      resultSpace.appendChild(finalResult);
+      winCounter=0;
+      loseCounter=0;
+    }
+    if (loseCounter===5) {
+      finalResult.innerHTML="You lost!";
+      resultSpace.appendChild(finalResult);
+      winCounter=0;
+      loseCounter=0;
+    }
 
+  // reset
+  randomNumber=0;
   })
+
+
 });
+
